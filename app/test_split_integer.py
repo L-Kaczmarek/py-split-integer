70e492769e98e02a5a66713b617e5142144edc7e
Łukasz Kaczmarek <Lukasz.kaczmarek1993@wp.pl>
2026-04-01 15:45:07 +0200

    'Fix'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,9 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(32, 4)
-    assert sum(result) == 32
-    assert len(result) == 4
+    split_integer(32, 4)
+    assert sum(split_integer(32, 4)) == 32
+    assert len(split_integer(32, 4)) == 4
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
